8114b07e03f16d430e9e7a136e2c722304eb365e
Kevin <695945516@qq.com>
2022-01-07 13:06:27 +0800

    Update train_inceptionv3_ilsvrc.sh

--- scripts/train_inceptionv3_ilsvrc.sh
+++ scripts/train_inceptionv3_ilsvrc.sh
@@ -19,8 +19,8 @@ python train_sst.py \
     --sa_head=8 \
     --sa_neu_num=768 \
     --watch_cam \
-    --snapshot_dir=../snapshots/ilsvrc/inceptionv3_sos+sa_v3_#21 \
-    --log_dir=../log/ilsvrc/inceptionv3_sos+sa_v3_#21 \
+    --snapshot_dir=../snapshots/ilsvrc/inceptionv3_sos+sa_v3_#22 \
+    --log_dir=../log/ilsvrc/inceptionv3_sos+sa_v3_#22 \
     --load_finetune=True \
     --pretrained_model=ilsvrc_epoch_20.pth.tar \
     --pretrained_model_dir=../snapshots/ilsvrc/inceptionv3_cls \
@@ -35,16 +35,16 @@ python train_sst.py \
     --cls_lr=0.001 \
     --sa_lr=0.01 \
     --sos_lr=0.00005 \
-    --scg_fosc_th=0.1 \
-    --scg_sosc_th=0.5 \
+    --scg_fosc_th=0.4 \
+    --scg_sosc_th=4 \
     --sa_start=0 \
     --sos_fg_th=0.4 \
     --sos_bg_th=0.3 \
     --sos_loss_weight=0.5 \
-    --ram_th_bg=0.1 \
-    --ram_bg_fg_gap=0.8 \
+    --ram_th_bg=0.4 \
+    --ram_bg_fg_gap=0.1 \
     --ram_start=0 \
-    --ra_loss_weight=0.1 \
+    --ra_loss_weight=0.5 \
     --mode=sos+sa_v3 \
     --ram \
 
