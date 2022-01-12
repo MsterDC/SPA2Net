915ff6c46d91115ab0c2ff7a1a32aee24d6ed662
Kevin <695945516@qq.com>
2022-01-12 13:18:10 +0800

    Add files via upload

--- scripts/train_inceptionv3_ilsvrc.sh
+++ scripts/train_inceptionv3_ilsvrc.sh
@@ -12,28 +12,28 @@ python train_sst.py \
     --sos_start=0 \
     --sa_head=8 \
     --watch_cam \
-    --snapshot_dir=../snapshots/ilsvrc/inceptionv3_sos+sa_v3_#18 \
-    --log_dir=../log/ilsvrc/inceptionv3_sos+sa_v3_#18 \
-    --num_workers=32 \
-    --resume=True \
+    --snapshot_dir=../snapshots/ilsvrc/inceptionv3_sos+sa_v3_#24 \
+    --log_dir=../log/ilsvrc/inceptionv3_sos+sa_v3_#24 \
+    --num_workers=48 \
+    --resume=False \
     --restore_from=ilsvrc_epoch_4.pth.tar \
-    --load_finetune=False \
+    --load_finetune=True \
     --finetuned_model_dir=../snapshots/ilsvrc/inceptionv3_cls \
     --finetuned_model=ilsvrc_epoch_20.pth.tar \
     --batch_size=64 \
     --gpus=0,1,2,3 \
     --epoch=20 \
-    --warmup=False \
+    --warmup=True \
     --warmup_fun=gra \
-    --decay_point=5,17 \
+    --decay_point=12,14 \
     --decay_module=bb,cls,sa\;bb,cls,sa \
     --lr=0.001 \
     --cls_lr=0.001 \
     --sa_lr=0.01 \
-    --sos_lr=0.0005 \
+    --sos_lr=0.00005 \
     --scg_fosc_th=0.1 \
     --scg_sosc_th=0.5 \
-    --sa_start=0 \
+    --sa_start=3 \
     --sa_use_edge=True \
     --sos_fg_th=0.5 \
     --sos_bg_th=0.3 \
@@ -41,6 +41,7 @@ python train_sst.py \
     --ram \
     --ram_th_bg=0.4 \
     --ram_bg_fg_gap=0.1 \
-    --ram_start=0 \
+    --ram_start=3 \
     --ram_loss_weight=0.5 \
+    --spa_loss=True \
     --mode=sos+sa_v3 \
